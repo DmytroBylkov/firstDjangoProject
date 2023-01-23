f7196ef87af3a7a12d5637a22a10ee5ed5570779
DmytroBylkov <bylkovd@gmail.com>
2023-01-23 06:59:55 +0200

    "added pathes home, progression, greeting and fib"

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -15,7 +15,45 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+
+def home_page(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>Home page</h1>")
+
+def progression_page(request: HttpRequest, start: int, count: int, step: int) -> HttpResponse:
+    result = [int(start)]
+    for i in range(int(count) - 1):
+        result.append(result[i] + int(step))
+    return HttpResponse(f"<p>{result}</p>")
+
+def fib_page(request:HttpRequest, n: int) -> HttpResponse:
+    n = int(n)
+    if n <= 0:
+        fib_sum = 0
+    elif n == 1:
+        fib_sum = 1
+    else:
+        fib1 = 0
+        fib2 = 1
+        i = 2
+        while i < n:
+            fib_sum = fib2 + fib1
+            fib1 = fib2
+            fib2 = fib_sum
+            i += 1
+    return HttpResponse(f"<h1>{fib_sum}</h1>")
+
+def greeting_page(request: HttpRequest, name: str) -> HttpResponse:
+    return HttpResponse(f"<h3>Greeting, {name}</h3>")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('homepage/', home_page),
+    path('home/', home_page),
+    path('', home_page),
+    path('progression/<start>/<count>/<step>', progression_page),
+    path('fib/<n>', fib_page),
+    path('greeting/<name>', greeting_page)
 ]
